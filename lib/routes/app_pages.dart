023f9f09ba7410ac54a6aff334c1a9fda0ae83d4
Bakke Medina <bakke.medina@innova-ti.cl>
2026-05-21 10:30:52 -0500

    FIX ruta

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -5,7 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:poke_test/presentation/globals/controllers/navigator_key/navigator_key_gc.dart';
 import 'package:poke_test/routes/app_routes.dart/home_route.dart';
 import 'package:poke_test/routes/app_routes.dart/login_route.dart';
-import 'package:poke_test/routes/app_routes.dart/pokemon_detail.dart';
+import 'package:poke_test/routes/app_routes.dart/pokemon_detail_route.dart';
 import 'package:poke_test/routes/app_routes.dart/splash_route.dart';
 
 
